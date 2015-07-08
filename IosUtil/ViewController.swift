44e0f1fb6961a35fa0a8e1c24b6a846fe7be083f
fanhl <Dyguest0315>
2015-07-08 15:55:13 +0800

    Initial Commit

---
+++ IosUtil/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IosUtil
+//
+//  Created by 范鸿麟 on 15/7/8.
+//  Copyright (c) 2015年 范鸿麟. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
